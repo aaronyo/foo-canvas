5660e9ee5c2bf902d85e624289aabff53944e3c1
Aaron Boyd <aaron.t.boyd@gmail.com>
2018-07-15 14:41:17 -0700

    bootstrap using webpack-boilerplate

---
+++ app/index.js
@@ -0,0 +1,12 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.scss';
+import fp from 'lodash/fp';
+console.log(fp.map);
+
+// ================================
+// START YOUR APP HERE
+// ================================
