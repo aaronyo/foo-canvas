64bfb7562aacca74ba0a59c615e344edb2afe74c
Aaron Boyd <aaron.t.boyd@gmail.com>
2018-07-20 17:47:00 -0700

    add looping star field and static enemy ship

--- app/index.js
+++ app/index.js
@@ -71,6 +71,10 @@ function makeScene() {
       xVelocity: 0,
       rotation: 0,
     },
+    enemy: {
+      x: 1000,
+      y: 1000,
+    },
   };
 }
 
@@ -112,6 +116,8 @@ function rotateShip(ship, dir) {
 function moveShip(universe, ship) {
   return {
     ...ship,
+    //    y: ship.y + ship.yVelocity,
+    //    x: ship.x + ship.xVelocity,
     y: (ship.y + ship.yVelocity + universe.height) % universe.height,
     x: (ship.x + ship.xVelocity + universe.width) % universe.width,
   };
@@ -155,6 +161,10 @@ function makeKeyState() {
 }
 
 function makeBackground({ width, height }) {
+  const texture = PIXI.Texture.fromImage('assets/space.png');
+  const bg = new PIXI.TilingSprite(texture, width, height);
+  return [bg, texture];
+
   const gfx = new PIXI.Graphics();
   for (let i = 0; i <= width; i += 50) {
     const lwidth = i % width === 0 ? 10 : 1;
@@ -206,63 +216,80 @@ function main() {
   let scene = makeScene();
   const keyState = makeKeyState();
   // load the texture we need
-  PIXI.loader.add('ship', 'assets/ship.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const ship = new PIXI.Sprite(resources.ship.texture);
+  PIXI.loader
+    .add('ship', 'assets/ship-color.png')
+    .add('enemy', 'assets/ship-enemy.png')
+    .load((loader, resources) => {
+      // This creates a texture from a 'bunny.png' image
+      const ship = new PIXI.Sprite(resources.ship.texture);
+      const enemy = new PIXI.Sprite(resources.enemy.texture);
 
-    // Rotate around the center
-    ship.scale.x = 0.5;
-    ship.scale.y = 0.5;
-    ship.anchor.x = 0.5;
-    ship.anchor.y = 0.75;
-    ship.x = viewport.center.x;
-    ship.y = viewport.center.y;
-    // Add the ship to the scene we are building
-    const background = makeBackground(scene.universe);
-    app.stage.addChild(background);
-    app.stage.addChild(ship);
-    app.stage.addChild(text);
+      // Rotate around the center
+      ship.scale.x = 0.5;
+      ship.scale.y = 0.5;
+      ship.anchor.x = 0.5;
+      ship.anchor.y = 0.75;
+      ship.x = viewport.center.x;
+      ship.y = viewport.center.y;
+      enemy.scale.x = 0.5;
+      enemy.scale.y = 0.5;
+      enemy.anchor.x = 0.5;
+      enemy.anchor.y = 0.25;
+      // Add the ship to the scene we are building
+      const [background, texture] = makeBackground(viewport);
+      app.stage.addChild(background);
+      app.stage.addChild(ship);
+      app.stage.addChild(enemy);
+      app.stage.addChild(text);
 
-    app.ticker.add(delta => {
-      // each frame we spin the ship around a bit
-      //      ship.rotation += 0.01;
-      play();
-    });
+      app.ticker.add(delta => {
+        // each frame we spin the ship around a bit
+        //      ship.rotation += 0.01;
+        play();
+      });
 
-    const moveShip_ = _.partial(moveShip, scene.universe);
+      const moveShip_ = _.partial(moveShip, scene.universe);
 
-    function play() {
-      const shipUpdates = [
-        keyState.left.isDown && (ship => rotateShip(ship, -1)),
-        keyState.right.isDown && (ship => rotateShip(ship, 1)),
-        keyState.thrust.isDown && thrustShip,
-        moveShip_,
-      ];
+      function play() {
+        const shipUpdates = [
+          keyState.left.isDown && (ship => rotateShip(ship, -1)),
+          keyState.right.isDown && (ship => rotateShip(ship, 1)),
+          keyState.thrust.isDown && thrustShip,
+          moveShip_,
+        ];
 
-      //Use the ship's velocity to make it move
-      scene = updateScene(
-        'ship',
-        fp.flow(fp.filter(fp.identity, shipUpdates)),
-        scene,
-      );
+        //Use the ship's velocity to make it move
+        scene = updateScene(
+          'ship',
+          fp.flow(fp.filter(fp.identity, shipUpdates)),
+          scene,
+        );
 
-      background.x = viewport.width / 2 - scene.ship.x;
-      background.y = viewport.height / 2 - scene.ship.y;
-      handleUniverseEdge(background, ship, 'x', 'width');
-      handleUniverseEdge(background, ship, 'y', 'height');
+        background.tilePosition.x = viewport.width / 2 - scene.ship.x;
+        background.tilePosition.y = viewport.height / 2 - scene.ship.y;
 
-      ship.rotation = scene.ship.rotation;
-      text.text = `rot: ${util.round(
-        scene.ship.rotation,
-        2,
-      )}, xVel:${util.round(scene.ship.xVelocity, 2)}, yVel:${util.round(
-        scene.ship.yVelocity,
-        2,
-      )}, x:${util.round(scene.ship.x, 0)}, y:${util.round(scene.ship.y, 0)}
+        enemy.x = scene.enemy.x - scene.ship.x + viewport.width / 2;
+        enemy.y = scene.enemy.y - scene.ship.y + viewport.height / 2;
+
+        // if (Math.abs(
+        //   ((scene.ship.x - scene.enemy.x) < viewport.width / 2)
+        //     || ((scene.ship.y - scene.enemy.y) < viewport.height / 2))) {
+        // }
+        // handleUniverseEdge(background, ship, 'x', 'width');
+        // handleUniverseEdge(background, ship, 'y', 'height');
+
+        ship.rotation = scene.ship.rotation;
+        text.text = `rot: ${util.round(
+          scene.ship.rotation,
+          2,
+        )}, xVel:${util.round(scene.ship.xVelocity, 2)}, yVel:${util.round(
+          scene.ship.yVelocity,
+          2,
+        )}, x:${util.round(scene.ship.x, 0)}, y:${util.round(scene.ship.y, 0)}
 `;
-      //      console.log(sceneState.ship.yVelocity);
-    }
-  });
+        //      console.log(sceneState.ship.yVelocity);
+      }
+    });
 }
 
 window.onload = main;
